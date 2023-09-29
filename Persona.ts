0fb214b96cd553e27cc2c213b0fae70aa009ba40
Juancyn <alejandrovera_05@hotmail.com>
2023-09-28 20:59:38 -0500

    Update Persona.ts

--- Persona.ts
+++ Persona.ts
@@ -1,6 +1,35 @@
 class persona{
-    private nombre: string;
-    private apellido: string;
-    private cedula: number;
-    private estado_civil: string;
+    private _nombre: string;
+    public get nombre(): string {
+        return this._nombre;
+    }
+    public set nombre(value: string) {
+        this._nombre = value;
+    }
+    private _apellido: string;
+    public get apellido(): string {
+        return this._apellido;
+    }
+    public set apellido(value: string) {
+        this._apellido = value;
+    }
+    private _cedula: number;
+    public get cedula(): number {
+        return this._cedula;
+    }
+    public set cedula(value: number) {
+        this._cedula = value;
+    }
+    private _estado_civil: string;
+    public get estado_civil(): string {
+        return this._estado_civil;
+    }
+    public set estado_civil(value: string) {
+        this._estado_civil = value;
+    }
+
+ constructor(){ 
+        
+    }
+
 }
